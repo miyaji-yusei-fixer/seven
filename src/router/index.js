0db4cdb8a6826bd1392ada770847cba644b14b53
miyaji-yusei-fixer <miyaji.yusei@fixer.co.jp>
2023-11-06 19:47:47 +0900

    :tada: vue, vuetify, vue-router, vuex

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = new VueRouter({
+  /* これを入れることでURLから#が消える
+  参考：https://code-schools.com/vue-router-sharp-remove/
+  */
+  mode: 'history',
+  routes
+})
+
+export default router
